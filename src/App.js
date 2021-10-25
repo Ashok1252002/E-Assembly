324ceeb4bb4ed8d5508496b73e6c7f856f4e17f3
Ashok Raj <ashokrajkoribilli@gmail.com>
2021-10-25 22:17:38 +0530

    updated build-1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
 	return (
 		<div className="App">
-			<h1>Hello E-Assembly Team TEST-Deploy</h1>
+			<h1>Hello E-Assembly Team TEST-Deploy-1</h1>
 		</div>
 	);
 }
