74c566db88001370434121e35c9a25091c3a6c15
Ashok Raj <ashokrajkoribilli@gmail.com>
2021-10-25 22:21:35 +0530

    release/0.0.1/mummly

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
 	return (
 		<div className="App">
-			<h1>Hello E-Assembly Team TEST-Deploy-1</h1>
+			<h1>Hello E-Assembly Team TEST-Deploy-MUMMLY</h1>
 		</div>
 	);
 }
