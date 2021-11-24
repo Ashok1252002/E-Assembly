98e39bc6018b2ca1844617d9cc00efd2fc31f6c2
Ashok Raj <ashokrajkoribilli@gmail.com>
2021-11-24 23:17:48 +0530

    Added json server

--- src/Containers/App/index.js
+++ src/Containers/App/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../Header/index";
 import Footer from "../Footer/index";
+import Home from "../Home/index";
 
 function App() {
 	return (
 		<div>
 			<Header />
+			<Home />
 			<Footer />
 		</div>
 	);
